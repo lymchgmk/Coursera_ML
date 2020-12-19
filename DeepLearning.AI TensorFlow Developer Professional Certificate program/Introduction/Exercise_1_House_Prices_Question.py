560ee8b8fec026376e0d9dfb89d6b193596f445e
lymchgmk@gmail.com <lymchgmk@gmail.com>
2020-12-19 18:34:26 +0900

    after tensorflow

--- DeepLearning.AI TensorFlow Developer Professional Certificate program/Introduction/Exercise_1_House_Prices_Question.py
+++
@@ -1,9 +0,0 @@
-import tensorflow as tf
-import numpy as np
-from tensorflow import keras
-model = tf.keras.Sequential([keras.layers.Dense(units=1, input_shape=[1])])
-model.compile(optimizer='sgd', loss='mse')
-xs = np.array([1.0, 2.0, 3.0, 4.0, 5.0, 6.0], dtype=float)
-ys = np.array([1.0, 1.5, 2.0, 2.5, 3.0, 3.5], dtype=float)
-model.fit(xs, ys, epochs=100)
-print(model.predict([7.0]))
